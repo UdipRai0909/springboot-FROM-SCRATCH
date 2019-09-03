2484d043169b31bf36599c48729b6d705672418c
Udip Rai <udiprai28@gmail.com>
2019-09-04 02:51:20 +0545

    First Commit - Initiation of Project

---
+++ springboot.from-scratch/src/main/java/com/springboot/fromscratch/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.springboot.fromscratch.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World!";
+	}
+}
